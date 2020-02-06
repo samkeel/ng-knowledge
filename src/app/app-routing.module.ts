40b23e4a7f1869e65b8447363ed38891c574f78d
Sam <programming@vectors.net.au>
2020-02-06 19:46:08 +0800

    updated layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,8 @@ import { AuthGuard } from './user/auth.guard';
 
 
 const routes: Routes = [
-  {  
-    path: '', component: HomePageComponent
-  },
   {
-    path: 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+    path: '', loadChildren: () => import('./user/user.module').then(m => m.UserModule)
   },
   {
     path: 'docs',
