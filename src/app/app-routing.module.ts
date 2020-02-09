15ed7ea60689197e65cb9b9efb9cb7aa7fd6587f
Sam <programming@vectors.net.au>
2020-02-09 20:47:32 +0800

    route fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
     canActivate: [AuthGuard]
   },
   {
-    path: '**', loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+    path: '**', redirectTo: ''
   }
 ];
 
--- src/app/shared/route-animations.ts
+++ src/app/shared/route-animations.ts
@@ -1,17 +1,5 @@
 import { animate, style, transition, trigger } from '@angular/animations';
 
-export const routeFadeStateTrigger = trigger('routeFadeState', [
-  transition(':enter', [
-    style({
-      opacity: 0
-    }),
-    animate(300)
-  ]),
-  transition(':leave', animate(300, style({
-    opacity: 0
-  })))
-]);
-
 export const routeSlideStateTrigger = trigger('routeSlideState', [
   transition(':enter', [
     style({
