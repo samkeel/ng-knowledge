5cd7555addffc7af31f555c293b7ae00baeb9926
Sam <programming@vectors.net.au>
2020-02-09 12:14:37 +0800

    route animation added

--- src/app/docs/doc-home/doc-home.component.ts
+++ src/app/docs/doc-home/doc-home.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostBinding } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { BookService } from './book.service';
 import { DocModel } from './doc.model';
+import { routeSlideStateTrigger } from 'src/app/shared/route-animations';
 
 
 @Component({
   selector: 'app-doc-home',
   templateUrl: './doc-home.component.html',
-  styleUrls: ['./doc-home.component.scss']
+  styleUrls: ['./doc-home.component.scss'],
+  animations: [routeSlideStateTrigger]
 })
 export class DocHomeComponent implements OnInit, OnDestroy {
-  documentsList: DocModel[] = [];
+  @HostBinding('@routeSlideState') routeAnimation = true;
 
+  documentsList: DocModel[] = [];  
 
   constructor(private bookService: BookService, private db: AngularFirestore) { }
 
--- src/app/user/user-home/user-home.component.ts
+++ src/app/user/user-home/user-home.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { routeSlideStateTrigger } from 'src/app/shared/route-animations';
 
 @Component({
   selector: 'app-user-home',
   templateUrl: './user-home.component.html',
-  styleUrls: ['./user-home.component.scss']
+  styleUrls: ['./user-home.component.scss'],
+  animations: [routeSlideStateTrigger]
 })
 export class UserHomeComponent implements OnInit {
+  @HostBinding('@routeSlideState') routeAnimation = true;
 
   constructor(public afAuth: AngularFireAuth) { }
 
