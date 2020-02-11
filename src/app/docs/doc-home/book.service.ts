f360c37edaa1d20d1ee92fec339484e7158cc4ea
Sam <programming@vectors.net.au>
2020-02-11 08:27:35 +0800

    db link to books

--- src/app/docs/doc-home/book.service.ts
+++ src/app/docs/doc-home/book.service.ts
@@ -1,10 +1,18 @@
 import { Observable, of } from 'rxjs';
 import { DOCMODEL } from './mock-books';
 import { DocModel } from './doc.model';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+    providedIn: 'root'
+})
 
 export class BookService {
-    constructor(){}
-    getBooks(): Observable<DocModel[]> {
-        return of(DOCMODEL)
+    constructor(private db: AngularFirestore) { }
+
+    getBooks(): Observable<any[]> {
+        return this.db.collection('Docs').valueChanges();
     }
 }
